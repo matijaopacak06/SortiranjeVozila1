e108cec201d73c8a109f3430bfa953b8b7d9cb4c
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-11-13 08:51:23 +0100

    Add files via upload

---
+++ Sortiranje_vozila/Vozilo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortiranje_vozila
+{
+    internal class Vozilo
+    {
+        string Marka;
+        string Model;
+        int GodinaProizvodnje;
+        int Kilometraza;
+
+        public string Marka1 { get => Marka; set => Marka = value; }
+        public string Model1 { get => Model; set => Model = value; }
+        public int GodinaProizvodnje1 { get => GodinaProizvodnje; set => GodinaProizvodnje = value; }
+        public int Kilometraza1 { get => Kilometraza; set => Kilometraza = value; }
+    }
+}
